2acd0cf315c8b349945e32ea661e8e74369dd01b
Megex04 <miantolalfa04@gmail.com>
2022-09-12 13:06:35 -0500

    config data kafka

---
+++ notificacionkafka/src/main/java/pe/com/nttdata/kafka/NotificacionKafkaApplication.java
@@ -0,0 +1,7 @@
+package pe.com.nttdata.kafka;
+
+public class NotificacionKafkaApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ notificacionkafka/src/main/java/pe/com/nttdata/kafka/dao/INotificacionDao.java
@@ -0,0 +1,8 @@
+package pe.com.nttdata.kafka.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pe.com.nttdata.kafka.model.Notificacion;
+import pe.com.nttdata.kafka.model.Notificacion;
+
+public interface INotificacionDao extends JpaRepository<Notificacion,Integer> {
+}
