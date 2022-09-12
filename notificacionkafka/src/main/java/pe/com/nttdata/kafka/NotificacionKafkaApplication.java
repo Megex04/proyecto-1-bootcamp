a284687d70160cbf641266814bb14041cc5583a1
Megex04 <miantolalfa04@gmail.com>
2022-09-12 13:15:37 -0500

    config datav2 kafka

--- notificacionkafka/src/main/java/pe/com/nttdata/kafka/NotificacionKafkaApplication.java
+++ notificacionkafka/src/main/java/pe/com/nttdata/kafka/NotificacionKafkaApplication.java
@@ -1,7 +1,11 @@
 package pe.com.nttdata.kafka;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class NotificacionKafkaApplication {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(NotificacionKafkaApplication.class, args);
     }
 }
